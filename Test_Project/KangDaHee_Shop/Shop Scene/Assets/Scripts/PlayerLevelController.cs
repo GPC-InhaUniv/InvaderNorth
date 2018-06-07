79c347fbce34d6854765d7af6905dab4c54ad809
dahee <isc1645@gmail.com>
2018-06-07 14:50:18 +0900

    20180607_강다희_Shop UI 설정 및 업그레이드 적용

---
+++ Test_Project/KangDaHee_Shop/Shop Scene/Assets/Scripts/PlayerLevelController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLevelController : MonoBehaviour
+{
+    public Text playerLevelText;
+
+    public Text heartLevel;
+    public Text shotLevel;
+    public Text criticalLevel;
+
+    public int heartnum;
+    public int shotnum;
+    public int criticalnum;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+
+    public void ChangePlayerLevel()
+    {
+        heartnum = Int32.Parse(heartLevel.text);
+        shotnum = Int32.Parse(shotLevel.text);
+        criticalnum = Int32.Parse(criticalLevel.text);
+
+        playerLevelText.text = (heartnum + shotnum + criticalnum).ToString();
+    }
+}
---
+++ Test_Project/KangDaHee_Shop/Shop Scene/Assets/Scripts/UpgradeController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class UpgradeController : MonoBehaviour
+{
+    private int gearLevel;
+    public int upgradeMaxLevel;
+
+    public int levelResourceNum;
+
+    private int changedNum01;
+    private int changedNum02;
+    private int changedNum03;
+
+    public Image upgradeButton;
+
+    public Text levelText;
+    public Text levelResourceText;
+
+    public GameObject changedImage01;
+    public GameObject changedImage02;
+    public GameObject changedImage03;
+    public GameObject changedImage04;
+
+    private void Start()
+    {
+        changedNum01 = 5;
+        changedNum02 = 10;
+        changedNum03 = 15;
+    }
+
+    private void Update()
+    {
+    }
+
+    public void ChangeGear()
+    {
+        if (gearLevel < upgradeMaxLevel)
+        {
+            gearLevel++;
+            levelResourceNum = levelResourceNum + 500;
+
+            levelText.text = gearLevel.ToString();
+            levelResourceText.text = levelResourceNum.ToString();
+        }
+
+        if (gearLevel == changedNum01)
+        {
+            changedImage02.SetActive(true);
+            changedImage01.SetActive(false);
+        }
+
+        if (gearLevel == changedNum02)
+        {
+            changedImage03.SetActive(true);
+            changedImage02.SetActive(false);
+        }
+        if (gearLevel == changedNum03)
+        {
+            changedImage04.SetActive(true);
+            changedImage03.SetActive(false);
+        }
+
+        if (gearLevel == upgradeMaxLevel)
+        {
+            levelResourceText.text = "-";
+            upgradeButton.enabled = false;
+        }
+    }
+}
