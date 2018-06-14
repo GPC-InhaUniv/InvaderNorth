ec117cfdfc412e0b7666c2ff69b0b283abdc35ac
DulgiKim <dulgi0121@gmail.com>
2018-06-14 23:59:31 +0900

    PINT-55 20180614 김승환 Lobby Stage 병합

---
+++ Test_Project/KimSeungHwan_Intro_Loading/SpaceShooter/Assets/Scripts/Lobby/PopupController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupController : MonoBehaviour {
+
+    public Canvas PopUpWindow;
+    
+    private void Start()
+    {
+        PopUpWindow.enabled = false;
+    }
+
+    public void PopUp()
+    {
+        PopUpWindow.enabled = true;
+    }
+
+    public void Exit()
+    {
+        PopUpWindow.enabled = false;
+    }
+
+
+}
