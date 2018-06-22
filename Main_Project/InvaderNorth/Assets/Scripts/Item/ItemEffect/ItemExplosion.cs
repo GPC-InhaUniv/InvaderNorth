4685d4658e7ed9edac6581cae70ca006dc23f1d4
이신재 <magickane12@gmail.com>
2018-06-22 19:52:48 +0900

    PINT-36 20180622 이신재 Explosion 구현중

--- Main_Project/InvaderNorth/Assets/Scripts/Item/ItemEffect/ItemExplosion.cs
+++ Main_Project/InvaderNorth/Assets/Scripts/Item/ItemEffect/ItemExplosion.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 //폭발효과
 public class ItemExplosion : ItemEffectType
 {
-    [Header("BombExplosionObjects")]
-    [SerializeField]
     private GameObject ExplosionRange;
-    [SerializeField]
-    private GameObject BombExplosion;
-   
-  
-    private void Start()
-    {
-        
-    }
+    private GameObject FXBombExplosion;
+    private int BombEffectLimitTime;
     
+    private ItemExplosion()
+    {
+        BombEffectLimitTime = 20;
+        ExplosionRange = Resources.Load("Prefabs/BombExplosion") as GameObject;
+        FXBombExplosion = Resources.Load("Prefabs/FXBombExplosion") as GameObject;
+
+        ExplosionRange.SetActive(false);
+        FXBombExplosion.SetActive(false);
+    }
+
     public override void ApplyTheEffect()
     {
         base.ApplyTheEffect();
-        
         Explode();
     }
     
-    public void Explode()
+    //여기에 폭발범위, 폭발이펙트 활성화 / 이후 비활성화
+    IEnumerator Explode()
     {
-       
+        for (int i = 0; i <= BombEffectLimitTime; i++)
+        {
+            ExplosionRange.SetActive(true);
+            FXBombExplosion.SetActive(true);
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        ExplosionRange.SetActive(false);
+        FXBombExplosion.SetActive(false);
+        
     }
 }
