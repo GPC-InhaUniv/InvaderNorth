1f3cd7ba70b7fabdfff1877052e0e6ed636edb26
dahee <isc1645@gmail.com>
2018-06-22 18:38:03 +0900

    PINT-60 20180622 강다희 MaxLevel분리 및 View 분리 작업중-2

--- Main_Project/InvaderNorth/Assets/Scripts/ShopUI/UpgradeController.cs
+++ Main_Project/InvaderNorth/Assets/Scripts/ShopUI/UpgradeController.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System;
 
-public class UpgradeController : ShopStageView
+public class UpgradeController : MonoBehaviour
 {
     private int gearType; //Inspector에서 연결된, LevelText의 번호를 가져와 어떤 종류인지 확인하는 Level
     private int playerGearLevel; //매니저에서 가져온 아이템이 어떤 종류인지 확인하고 넣어주는 아이템의 Level
@@ -15,6 +15,9 @@ public class UpgradeController : ShopStageView
 
     [Header("Credit Imformation")]
     [SerializeField]
+    private int gearUpgradeMaxLevel; //아이템의 Max Level 정보
+
+    [SerializeField]
     private Text gearLevelText; //화면에서의 각 아이템 Level Text -> 0,1,2로 구별함
 
     [SerializeField]
@@ -25,12 +28,18 @@ public class UpgradeController : ShopStageView
 
     [Header("Gear Image")]
     [SerializeField]
-    private GameObject[] levelImages;
+    private GameObject imageChanged1;
 
-    private int[] changedConst = new int[4]; // gearLevel에 따른, 이미지
+    [SerializeField]
+    private GameObject imageChanged2;
 
-    [SerializeField] //이거 데이터 매니저에서 가져와서 안쓸 수도 있음.
-    private int gearUpgradeMaxLevel; //아이템의 Max Level 정보
+    [SerializeField]
+    private GameObject imageChanged3;
+
+    [SerializeField]
+    private GameObject imageChanged4;
+
+    private int[] changedConst = new int[4];
 
     //DataManager.datainstance 에 접근하면 사용 안함
     private struct UserInfo
@@ -55,6 +64,11 @@ public class UpgradeController : ShopStageView
 
     private void Start()
     {
+        changedConst[0] = gearUpgradeMaxLevel - (gearUpgradeMaxLevel - gearUpgradeMaxLevel / 5);
+        changedConst[1] = gearUpgradeMaxLevel - gearUpgradeMaxLevel / 2;
+        changedConst[2] = gearUpgradeMaxLevel - gearUpgradeMaxLevel / 5;
+        changedConst[3] = gearUpgradeMaxLevel;
+
         UserInfo userInfo = new UserInfo();
         userInfo.UserLevelValue();
 
@@ -76,11 +90,6 @@ public class UpgradeController : ShopStageView
             playerGearLevel = userInfo.critLevel;
             gearUpgradeMaxLevel = UserInfo.maxCritLevel;
         }
-
-        changedConst[0] = gearUpgradeMaxLevel - (gearUpgradeMaxLevel - gearUpgradeMaxLevel / 5);
-        changedConst[1] = gearUpgradeMaxLevel - gearUpgradeMaxLevel / 2;
-        changedConst[2] = gearUpgradeMaxLevel - gearUpgradeMaxLevel / 5;
-        changedConst[3] = gearUpgradeMaxLevel;
 
         gearLevelText.text = playerGearLevel.ToString();
         playerCreditText.text = HadCredit.ToString();
@@ -121,22 +130,22 @@ public class UpgradeController : ShopStageView
     {
         if (playerGearLevel <= changedConst[0])
         {
-            levelImages[0].SetActive(true);
+            imageChanged1.SetActive(true);
         }
         else if (playerGearLevel <= changedConst[1])
         {
-            levelImages[1].SetActive(true);
-            levelImages[0].SetActive(false);
+            imageChanged2.SetActive(true);
+            imageChanged1.SetActive(false);
         }
         else if (playerGearLevel < changedConst[2])
         {
-            levelImages[1].SetActive(true);
-            levelImages[2].SetActive(false);
+            imageChanged3.SetActive(true);
+            imageChanged2.SetActive(false);
         }
         else if (playerGearLevel < changedConst[3])
         {
-            levelImages[4].SetActive(true);
-            levelImages[2].SetActive(false);
+            imageChanged4.SetActive(true);
+            imageChanged3.SetActive(false);
         }
     }
 
